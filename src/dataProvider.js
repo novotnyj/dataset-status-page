9237e7993f79ba44925ed5d399a2a5e84d585c25
Jan Novotny <naj.yntovon@gmail.com>
2019-02-04 12:38:44 +0100

    Normalized date

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -28,6 +28,7 @@ function groupDays(data) {
             result[createdAt].cleanItemCount += item.cleanItemCount;
         } else {
             result[createdAt] = { ...item };
+            result[createdAt] = moment(item.createdAt).hour(12).minute(0).toISOString();
         }
     });
 
